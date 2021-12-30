5359572820d1050384328ea0b9425ce45acfb79e
Facu Mediotte <facumediotte@gmail.com>
2021-12-29 22:46:32 -0300

    flatMap operator

--- spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,37 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        flatMapExample();
+    }
+
+    public void flatMapExample() throws Exception {
+        List<String> usersList = new ArrayList<>();
+        usersList.add("Andres Guzman");
+        usersList.add("Facundo Mediotte");
+        usersList.add("María Fulana");
+        usersList.add("Diego Maradona");
+        usersList.add("Juan Mengano");
+        usersList.add("Bruce Lee");
+        usersList.add("Bruce Willis");
+
+        Flux.fromIterable(usersList)
+            .map(name -> new User(name.split(" ")[0].toUpperCase(), name.split(" ")[1].toUpperCase()))
+            .flatMap(user -> {
+                if("bruce".equalsIgnoreCase(user.getName())) {
+                    return Mono.just(user);
+                } else {
+                    return Mono.empty();
+                }
+            })
+            .map(user -> {
+                String name = user.getName().toLowerCase();
+                user.setName(name);
+                return user;
+            })
+            .subscribe(it -> LOGGER.info(it.toString()));
+    }
+
+    public void iterableExample() throws Exception {
         List<String> usersList = new ArrayList<>();
         usersList.add("Andres Guzman");
         usersList.add("Facundo Mediotte");
@@ -52,4 +84,5 @@ public class SpringBootReactorApplication implements CommandLineRunner {
             err -> LOGGER.error(err.getMessage()),
             () -> LOGGER.info("The observable's execution has finalized successfully!"));
     }
+
 }
