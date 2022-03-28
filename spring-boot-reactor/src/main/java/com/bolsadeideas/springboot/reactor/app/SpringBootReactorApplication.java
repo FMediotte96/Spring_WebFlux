dfc1c4c8cdca90c7c351043cefa03d1523003767
Facu Mediotte <facumediotte@gmail.com>
2022-03-27 23:37:46 -0300

    Infinite intervals with interval and delayElements

--- spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -14,6 +14,7 @@ import reactor.core.publisher.Mono;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
@@ -26,7 +27,25 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        exampleDelayElements();
+        exampleInfiniteInterval();
+    }
+
+    public void exampleInfiniteInterval() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Flux.interval(Duration.ofSeconds(1))
+            .doOnTerminate(latch::countDown)
+            .flatMap(i -> {
+                if (i >= 5) {
+                    return Flux.error(new InterruptedException("Solo, hasta 5!"));
+                }
+                return Flux.just(i);
+            })
+            .map(i -> "Hola " + i)
+            .retry(2)
+            .subscribe(LOGGER::info, e -> LOGGER.error(e.getMessage()));
+
+        latch.await();
     }
 
     public void exampleDelayElements() {
