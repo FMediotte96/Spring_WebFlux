ad33c1a4a610e93a756b3be6825d3927b1300720
Facu Mediotte <facumediotte@gmail.com>
2022-03-28 23:58:49 -0300

    Spring-boot-webflux

---
+++ spring-boot-webflux/src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bolsadeideas.springboot.webflux.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
