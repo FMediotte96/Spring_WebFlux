a2080a843bf816661c106ba92cb2169c0e16d1bb
Facu Mediotte <facumediotte@gmail.com>
2022-04-04 09:43:48 -0300

    WebTestClient

--- spring-boot-webflux-apirest/src/test/java/com/apirest/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ spring-boot-webflux-apirest/src/test/java/com/apirest/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,15 +1,29 @@
 package com.apirest.springboot.webflux.app;
 
+import com.apirest.springboot.webflux.app.models.documents.Producto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
 
+    @Autowired
+    private WebTestClient client;
+
     @Test
-    void contextLoads() {
-        Assertions.assertTrue(true);
+    void listarTest() {
+        client.get()
+            .uri("/api/v2/productos")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .expectBodyList(Producto.class)
+            .hasSize(9);
     }
 
 }
