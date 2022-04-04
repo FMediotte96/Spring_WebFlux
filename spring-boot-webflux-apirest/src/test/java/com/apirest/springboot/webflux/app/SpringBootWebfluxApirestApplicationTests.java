28cd635fe1ca71e5d56106a8fba6688cab5341a5
Facu Mediotte <facumediotte@gmail.com>
2022-04-04 18:48:40 -0300

    Test endpoint eliminar

--- spring-boot-webflux-apirest/src/test/java/com/apirest/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ spring-boot-webflux-apirest/src/test/java/com/apirest/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -135,4 +135,24 @@ class SpringBootWebfluxApirestApplicationTests {
             .jsonPath("$.nombre").isEqualTo("Asus Notebook")
             .jsonPath("$.categoria.nombre").isEqualTo("Electrónico");
     }
+
+    @Test
+    void eliminarTest() {
+        Producto producto = service.findByNombre("Mica Cómoda 5 cajones").block();
+
+        assert producto != null;
+        client.delete()
+            .uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+            .exchange()
+            .expectStatus().isNoContent()
+            .expectBody()
+            .isEmpty();
+
+        client.get()
+            .uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+            .exchange()
+            .expectStatus().isNotFound()
+            .expectBody()
+            .isEmpty();
+    }
 }
