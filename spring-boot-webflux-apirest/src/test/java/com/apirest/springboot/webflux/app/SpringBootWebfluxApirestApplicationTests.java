3184b9b00663aaba296ecedc7a280e9bf73dd286
Facu Mediotte <facumediotte@gmail.com>
2022-04-04 09:53:25 -0300

    Test endpoint with consumeWith

--- spring-boot-webflux-apirest/src/test/java/com/apirest/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ spring-boot-webflux-apirest/src/test/java/com/apirest/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
@@ -23,7 +26,16 @@ class SpringBootWebfluxApirestApplicationTests {
             .expectStatus().isOk()
             .expectHeader().contentType(MediaType.APPLICATION_JSON)
             .expectBodyList(Producto.class)
-            .hasSize(9);
+            .consumeWith(response -> {
+                List<Producto> productos = response.getResponseBody();
+                assert productos != null;
+                productos.forEach(p -> {
+                    System.out.println(p.getNombre());
+                });
+
+                Assertions.assertTrue(productos.size() > 0);
+            });
+        //.hasSize(9);
     }
 
 }
