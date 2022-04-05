650ecac6e3a7c5e3a4bbfe57f6d464dcea120eae
Facu Mediotte <facumediotte@gmail.com>
2022-04-05 14:20:34 -0300

    Creando servidor de registro Eureka

---
+++ spring-boot-webflux-eureka-server/src/test/java/com/server/springboot/webflux/eureka/app/SpringBootWebfluxEurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.server.springboot.webflux.eureka.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
